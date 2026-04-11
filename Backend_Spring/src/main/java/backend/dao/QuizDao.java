1ad4c812e64c81a641f324f4f9485dc86e28f00b
EddylianOrigin <sylvaineddy22@gmail.com>
2026-04-11 17:17:26 +0200

    add frontend repo and delete files

---
+++ Backend_Spring/src/main/java/backend/dao/QuizDao.java
@@ -0,0 +1,7 @@
+package backend.dao;
+
+import backend.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+}
---
+++ Backend_Spring/src/main/java/backend/model/Response.java
@@ -0,0 +1,11 @@
+package backend.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer Id;
+    private String response;
+}
